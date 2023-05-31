c69f33e5a00df5ebda9dedba9a0734aa748556e4
AntiD2ta <mtpotro41@gmail.com>
2023-05-31 15:36:37 +0100

    style: Apply gofumpt to new changes

--- internal/docker/docker_test.go
+++ internal/docker/docker_test.go
@@ -486,7 +486,6 @@ func TestWaitExitCh(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
 			ctrl := gomock.NewController(t)
 			dockerClient := mocks.NewMockAPIClient(ctrl)
 			defer ctrl.Finish()

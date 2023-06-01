d8c99e98f426bcbad3e4389ad4936d335b201aa6
adriantpaez <adriantpaez@gmail.com>
2023-06-01 15:23:35 +0200

    doc: add doc to public objects

--- internal/pkgmngr/package.go
+++ internal/pkgmngr/package.go
@@ -10,14 +10,20 @@ const (
 	checksumFileName = "checksum.txt"
 )
 
+// PackageManager is used to interact with an AVS node software package at the given
+// path.
 type PackageManager struct {
 	path string
 }
 
+// NewPackageManager creates a new PackageManager instance for the given package path.
 func NewPackageManager(path string) *PackageManager {
 	return &PackageManager{path: path}
 }
 
+// Check checks the package for validity. It returns an error if the package is invalid.
+// It checks the existence of some required files and directories and computes the
+// checksums comparing them with the ones listed in the checksum.txt file.
 func (p *PackageManager) Check() error {
 	if err := checkPackageDirExist(p.path, pkgDirName); err != nil {
 		return err

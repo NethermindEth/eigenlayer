f15295113bbed5be7721274b402df67dbad4284c
AntiD2ta <mtpotro41@gmail.com>
2023-06-08 14:50:03 +0100

    test: Update manifest unit tests

--- internal/package_handler/manifest_test.go
+++ internal/package_handler/manifest_test.go
@@ -21,27 +21,33 @@ func TestManifestValidate(t *testing.T) {
 	}{
 		{
 			name:      "Full OK Manifest",
-			filePath:  "full-ok/manifest.yml",
+			filePath:  "full-ok/pkg/manifest.yml",
 			wantError: "",
 		},
 		{
 			name:      "Invalid Fields Manifest",
-			filePath:  "invalid-fields/manifest.yml",
+			filePath:  "invalid-fields/pkg/manifest.yml",
 			wantError: "Invalid hardware requirements -> invalid fields: hardware_requirements.min_cpu_cores -> (negative value), hardware_requirements.min_ram -> (negative value), hardware_requirements.min_free_space -> (negative value).  Invalid plugin -> invalid fields: plugin.git -> (invalid git url), plugin.image -> (invalid docker image).  ",
 		},
 		{
 			name:      "Minimal Manifest",
-			filePath:  "minimal/manifest.yml",
+			filePath:  "minimal/pkg/manifest.yml",
 			wantError: "",
 		},
 		{
 			name:      "Missing Fields Manifest",
-			filePath:  "missing-fields/manifest.yml",
+			filePath:  "missing-fields/pkg/manifest.yml",
 			wantError: "Invalid manifest file -> missing fields: version, node_version, name, upgrade, profiles. ",
+		},
+		{
+			name:      "Missing Fields Manifest in profile",
+			filePath:  "missing-fields-profile/pkg/manifest.yml",
+			wantError: "Invalid manifest file -> missing fields: version, node_version, name, upgrade.    Invalid profile 1 -> missing fields: name. ",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Helper()
 			data, err := os.ReadFile(filepath.Join(testDir, "manifests", tt.filePath))
 			if err != nil {
 				t.Fatalf("failed reading data from yaml file: %s", err)

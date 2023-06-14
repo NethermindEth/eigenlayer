38696e680c99e42230f984acb9a847018894f797
adriantpaez <adriantpaez@gmail.com>
2023-06-14 10:26:05 +0200

    feat: add default and help to prompter

--- internal/prompter/prompter.go
+++ internal/prompter/prompter.go
@@ -12,10 +12,12 @@ func SelectVersion(options []string) (string, error) {
 	return selectString("Select a version:", options)
 }
 
-func InputString(prompt string, validator func(string) error) (string, error) {
+func InputString(prompt, defValue, help string, validator func(string) error) (string, error) {
 	var result string
 	p := &survey.Input{
 		Message: prompt,
+		Default: defValue,
+		Help:    help,
 	}
 	err := survey.AskOne(p, &result, survey.WithValidator(func(ans interface{}) error {
 		if err := validator(ans.(string)); err != nil {

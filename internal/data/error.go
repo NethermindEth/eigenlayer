49d4acd4aba949de2792b1c38ce13c673b13c1eb
adriantpaez <adriantpaez@gmail.com>
2023-06-14 14:30:56 +0200

    test: add test case to RemoveInstance

--- internal/data/error.go
+++ internal/data/error.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrInstanceAlreadyExists = errors.New("instance already exists")
+	ErrInstanceNotFound      = errors.New("instance not found")
 	ErrInvalidInstance       = errors.New("invalid instance")
 	ErrInvalidInstanceDir    = errors.New("invalid instance directory")
 )

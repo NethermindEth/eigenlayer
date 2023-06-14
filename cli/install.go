9915182c6493b4da3240b8cde3f022880e0c16f0
adriantpaez <adriantpaez@gmail.com>
2023-06-15 00:49:21 +0200

    feat: update install command to merge options

--- cli/install.go
+++ cli/install.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/NethermindEth/eigen-wiz/internal/package_handler"
 	"github.com/NethermindEth/eigen-wiz/internal/prompter"
 	"github.com/NethermindEth/eigen-wiz/pkg/daemon"
 	"github.com/spf13/cobra"
@@ -29,7 +30,7 @@ func InstallCmd(d daemon.Daemon) *cobra.Command {
 			if err != nil {
 				return err
 			}
-			fmt.Println("Pulling package...")
+			fmt.Println("Pulling package...") // TODO: this is for debugging purpose, remove it later when we have a logger
 			pullResponse, err := d.Pull(&daemon.PullOptions{
 				URL:     url,
 				Version: version,
@@ -38,26 +39,26 @@ func InstallCmd(d daemon.Daemon) *cobra.Command {
 			if err != nil {
 				return err
 			}
-			fmt.Println("Package pulled successfully.")
-			profileNames := make([]string, 0, len(pullResponse.Profiles))
-			for k := range pullResponse.Profiles {
-				profileNames = append(profileNames, k)
-			}
-			selectedProfile, err := prompter.SelectProfile(profileNames)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("Selected profile: %s\n", selectedProfile)
-			profileOptions := pullResponse.Profiles[selectedProfile]
-			for _, option := range profileOptions {
-				_, err := prompter.InputString(option.Name(), option.Default(), option.Help(), func(s string) error {
-					return option.Set(s)
-				})
+			fmt.Println("Package pulled successfully.") // TODO: this is for debugging purpose, remove it later when we have a logger
+			for {
+				fillResult, err := fillOptions(destDir, pullResponse.Profiles)
 				if err != nil {
 					return err
 				}
+				// TODO: those are for debugging purpose, remove them later when we have a logger
+				fmt.Println("Profile: " + fillResult.profile)
+				fmt.Println("ENV variables to be set:")
+				for k, v := range fillResult.envVariables {
+					fmt.Printf("  %s=%s\n", k, v)
+				}
+				ok, err := prompter.Confirm("Do you want to continue with the selected options?")
+				if err != nil {
+					return err
+				}
+				if ok {
+					break
+				}
 			}
-			fmt.Printf("%+v\n", profileOptions)
 			return nil
 		},
 	}
@@ -65,4 +66,44 @@ func InstallCmd(d daemon.Daemon) *cobra.Command {
 	cmd.Flags().StringVarP(&version, "version", "v", "", "version to install. If not specified, the latest version will be installed.")
 	cmd.Flags().StringVarP(&tag, "tag", "t", "default", "tag to use for the new instance name")
 	return &cmd
+}
+
+type fillOptionsResult struct {
+	profile      string
+	envVariables map[string]string
+}
+
+func fillOptions(pullDir string, profiles map[string][]daemon.Option) (*fillOptionsResult, error) {
+	profileNames := make([]string, 0, len(profiles))
+	for k := range profiles {
+		profileNames = append(profileNames, k)
+	}
+	selectedProfile, err := prompter.SelectProfile(profileNames)
+	if err != nil {
+		return nil, err
+	}
+	profileOptions := profiles[selectedProfile]
+	for _, option := range profileOptions {
+		_, err := prompter.InputString(option.Name(), option.Default(), option.Help(), func(s string) error {
+			return option.Set(s)
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	pkgHandler := package_handler.NewPackageHandler(pullDir)
+	pkgDotEnv, err := pkgHandler.DotEnv(selectedProfile)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, option := range profileOptions {
+		pkgDotEnv[option.Target()] = option.Value()
+	}
+
+	return &fillOptionsResult{
+		profile:      selectedProfile,
+		envVariables: pkgDotEnv,
+	}, nil
 }

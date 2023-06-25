af795465bb61710b530a7ac6707da9dd52af7bbc
AntiD2ta <mtpotro41@gmail.com>
2023-06-25 11:25:02 +0000

    feat: Add Node_Exporter service

---
+++ internal/monitoring/services/node_exporter/service.go
@@ -0,0 +1,34 @@
+package node_exporter
+
+import (
+	"github.com/NethermindEth/egn/internal/monitoring"
+	"github.com/NethermindEth/egn/internal/monitoring/services/types"
+)
+
+var _ monitoring.ServiceAPI = &NodeExporterService{}
+
+type NodeExporterService struct{}
+
+func NewNodeExporter() *NodeExporterService {
+	return &NodeExporterService{}
+}
+
+func (n *NodeExporterService) Init(opts types.ServiceOptions) error {
+	return nil
+}
+
+func (n *NodeExporterService) AddTarget(endpoint string) error {
+	return nil
+}
+
+func (n *NodeExporterService) RemoveTarget(endpoint string) error {
+	return nil
+}
+
+func (n *NodeExporterService) DotEnv() map[string]string {
+	return dotEnv
+}
+
+func (n *NodeExporterService) Setup(options map[string]string) error {
+	return nil
+}

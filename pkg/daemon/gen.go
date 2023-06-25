51f6a8777c9ad9091d7d23ca4df961c4b5ee9863
AntiD2ta <mtpotro41@gmail.com>
2023-06-25 11:28:50 +0000

    feat: Update daemon
    Updates:
    - Update daemon with new afero and Locker
    - Add Monitoring Manager interface
    - Add Init method to initialize Daemon

--- pkg/daemon/gen.go
+++ pkg/daemon/gen.go
@@ -2,3 +2,4 @@ package daemon
 
 //go:generate mockgen -destination=./mocks/daemon.go -package=mocks github.com/NethermindEth/egn/pkg/daemon Daemon
 //go:generate mockgen -destination=./mocks/option.go -package=mocks github.com/NethermindEth/egn/pkg/daemon Option
+//go:generate mockgen -destination=./mocks/monitoring_manager.go -package=mocks github.com/NethermindEth/egn/pkg/daemon MonitoringManager
---
+++ pkg/daemon/monitoring.go
@@ -0,0 +1,20 @@
+package daemon
+
+import "github.com/NethermindEth/egn/internal/common"
+
+type MonitoringManager interface {
+	// InitStack initializes the monitoring stack.
+	InitStack() error
+	// AddTarget adds a new target to the monitoring stack.
+	AddTarget(endpoint string) error
+	// RemoveTarget removes a target from the monitoring stack.
+	RemoveTarget(endpoint string) error
+	// Status returns the status of the monitoring stack.
+	Status() (common.Status, error)
+	// InstallationStatus returns the installation status of the monitoring stack.
+	InstallationStatus() (common.Status, error)
+	// Run runs the monitoring stack.
+	Run() error
+	// Stop stops the monitoring stack.
+	Stop() error
+}

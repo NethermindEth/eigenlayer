25a202fda681e1364ffb240a907318082bd66edd
AntiD2ta <mtpotro41@gmail.com>
2023-06-25 11:22:56 +0000

    feat: Define monitoring interfaces

---
+++ internal/monitoring/constants.go
@@ -0,0 +1,11 @@
+package monitoring
+
+const (
+	PrometheusServiceName     = "prometheus"
+	PrometheusContainerName   = "egn_prometheus"
+	GrafanaServiceName        = "grafana"
+	GrafanaContainerName      = "egn_grafana"
+	NodeExporterServiceName   = "node_exporter"
+	NodeExporterContainerName = "egn_node_exporter"
+	monitoringPath            = "monitoring"
+)
---
+++ internal/monitoring/service.go
@@ -0,0 +1,22 @@
+package monitoring
+
+import "github.com/NethermindEth/egn/internal/monitoring/services/types"
+
+// ServiceAPI defines the interface for managing a monitoring service. It provides methods for
+// adding and removing targets, retrieving environment variables, setting up the service, and initializing the service.
+type ServiceAPI interface {
+	// AddTarget adds a new target to the service's configuration given the endpoint of the new node.
+	AddTarget(endpoint string) error
+
+	// RemoveTarget removes a target from the service's configuration given the endpoint of the node to be removed.
+	RemoveTarget(endpoint string) error
+
+	// DotEnv returns a map of the service's environment variables and their default values.
+	DotEnv() map[string]string
+
+	// Setup configures the service given a map of options. The options should include the values for the environment variables.
+	Setup(options map[string]string) error
+
+	// Init initializes the service with the given ServiceOptions.
+	Init(types.ServiceOptions) error
+}

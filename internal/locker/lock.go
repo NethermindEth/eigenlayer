00cfed175fd77e264e591d42f4d0c27fc795a175
AntiD2ta <mtpotro41@gmail.com>
2023-06-25 11:14:26 +0000

    feat: Add Locker

---
+++ internal/locker/lock.go
@@ -0,0 +1,35 @@
+package locker
+
+import "github.com/gofrs/flock"
+
+type Locker interface {
+	New(path string) Locker
+	Lock() error
+	Unlock() error
+	Locked() bool
+}
+
+type FLock struct {
+	locker *flock.Flock
+}
+
+func NewFLock() Locker {
+	return &FLock{}
+}
+
+func (l *FLock) New(path string) Locker {
+	l.locker = flock.New(path)
+	return l
+}
+
+func (l *FLock) Lock() error {
+	return l.locker.Lock()
+}
+
+func (l *FLock) Unlock() error {
+	return l.locker.Unlock()
+}
+
+func (l *FLock) Locked() bool {
+	return l.locker.Locked()
+}

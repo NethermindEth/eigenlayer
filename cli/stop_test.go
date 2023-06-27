31c574d6659a541996d2d185f0775671eabc8721
adriantpaez <adriantpaez@gmail.com>
2023-06-27 13:36:05 +0000

    test: stop command

---
+++ cli/stop_test.go
@@ -0,0 +1,65 @@
+package cli
+
+import (
+	"errors"
+	"testing"
+
+	daemonMock "github.com/NethermindEth/egn/cli/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStop(t *testing.T) {
+	ts := []struct {
+		name   string
+		args   []string
+		err    error
+		mocker func(d *daemonMock.MockDaemon)
+	}{
+		{
+			name:   "no arguments",
+			args:   []string{},
+			err:    errors.New("accepts 1 arg(s), received 0"),
+			mocker: nil,
+		},
+		{
+			name:   "more than one argument",
+			args:   []string{"arg1", "arg2"},
+			err:    errors.New("accepts 1 arg(s), received 2"),
+			mocker: nil,
+		},
+		{
+			name: "valid arguments, and stop success",
+			args: []string{"mock-avs-default"},
+			err:  nil,
+			mocker: func(d *daemonMock.MockDaemon) {
+				d.EXPECT().Stop("mock-avs-default").Return(nil)
+			},
+		},
+		{
+			name: "valid arguments, and stop error",
+			args: []string{"mock-avs-default"},
+			err:  errors.New("stop error"),
+			mocker: func(d *daemonMock.MockDaemon) {
+				d.EXPECT().Stop("mock-avs-default").Return(errors.New("stop error"))
+			},
+		},
+	}
+	for _, tt := range ts {
+		t.Run(tt.name, func(t *testing.T) {
+			controller := gomock.NewController(t)
+			d := daemonMock.NewMockDaemon(controller)
+			if tt.mocker != nil {
+				tt.mocker(d)
+			}
+
+			stopCmd := StopCmd(d)
+			stopCmd.SetArgs(tt.args)
+			err := stopCmd.Execute()
+
+			if tt.err != nil {
+				assert.EqualError(t, err, tt.err.Error())
+			}
+		})
+	}
+}

2149f9ba8444fddba4d725eff5d33b02d2ea8c09
adriantpaez <adriantpaez@gmail.com>
2023-07-04 12:49:41 +0000

    test: add monitoring not re-installed test case

--- e2e/monitoring_stack_test.go
+++ e2e/monitoring_stack_test.go
@@ -1,6 +1,7 @@
 package e2e
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -11,9 +12,12 @@ import (
 	"testing"
 	"time"
 
+	"github.com/docker/docker/client"
 	gapi "github.com/grafana/grafana-api-golang-client"
 	"github.com/stretchr/testify/assert"
 )
+
+const monitoringWaitTime = 20 * time.Second
 
 func TestMonitoringStack_Init(t *testing.T) {
 	// Prepare E2E test case
@@ -28,11 +32,89 @@ func TestMonitoringStack_Init(t *testing.T) {
 	err = cmd.Run()
 	assert.NoError(t, err)
 
+	// Wait for monitoring stack to be ready
+	time.Sleep(monitoringWaitTime)
+
 	checkMonitoringStack(t)
+	checkMonitoringInit(t)
+}
+
+func TestMonitoringStack_NotReinstalled(t *testing.T) {
+	// Prepare E2E test case
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	e2eTest := NewE2ETestCase(t, filepath.Dir(wd))
+	defer e2eTest.Cleanup()
+
+	cmd := exec.Command(e2eTest.EgnPath(), "--help")
+	err = cmd.Run()
+	assert.NoError(t, err)
 
 	// Wait for monitoring stack to be ready
-	time.Sleep(15 * time.Second)
+	time.Sleep(monitoringWaitTime)
 
+	checkMonitoringStack(t)
+	checkMonitoringInit(t)
+
+	// Docker client
+	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dockerClient.Close()
+	var (
+		grafanaContainerID      string
+		prometheusContainerID   string
+		nodeExporterContainerID string
+	)
+	// Check grafana container is running
+	grafanaContainer, err := dockerClient.ContainerInspect(context.Background(), "egn_grafana")
+	assert.NoError(t, err)
+	assert.True(t, grafanaContainer.State.Running, "grafana container is not running")
+	grafanaContainerID = grafanaContainer.ID
+
+	// Check prometheus container is running
+	prometheusContainer, err := dockerClient.ContainerInspect(context.Background(), "egn_prometheus")
+	assert.NoError(t, err)
+	assert.True(t, prometheusContainer.State.Running, "prometheus container is not running")
+	prometheusContainerID = prometheusContainer.ID
+
+	// Check node-exporter container is running
+	nodeExporterContainer, err := dockerClient.ContainerInspect(context.Background(), "egn_node_exporter")
+	assert.NoError(t, err)
+	assert.True(t, nodeExporterContainer.State.Running, "node-exporter container is not running")
+	nodeExporterContainerID = nodeExporterContainer.ID
+
+	// Run egn command again
+	cmd = exec.Command(e2eTest.EgnPath(), "--help")
+	err = cmd.Run()
+	assert.NoError(t, err)
+
+	// Check grafana container is running
+	grafanaContainer, err = dockerClient.ContainerInspect(context.Background(), "egn_grafana")
+	assert.NoError(t, err)
+	assert.True(t, grafanaContainer.State.Running, "grafana container is not running")
+	assert.Equal(t, grafanaContainerID, grafanaContainer.ID, "grafana container ID has changed")
+
+	// Check prometheus container is running
+	prometheusContainer, err = dockerClient.ContainerInspect(context.Background(), "egn_prometheus")
+	assert.NoError(t, err)
+	assert.True(t, prometheusContainer.State.Running, "prometheus container is not running")
+	assert.Equal(t, prometheusContainerID, prometheusContainer.ID, "prometheus container ID has changed")
+
+	// Check node-exporter container is running
+	nodeExporterContainer, err = dockerClient.ContainerInspect(context.Background(), "egn_node_exporter")
+	assert.NoError(t, err)
+	assert.True(t, nodeExporterContainer.State.Running, "node-exporter container is not running")
+	assert.Equal(t, nodeExporterContainerID, nodeExporterContainer.ID, "node-exporter container ID has changed")
+
+	checkMonitoringStack(t)
+	checkMonitoringInit(t)
+}
+
+func checkMonitoringInit(t *testing.T) {
 	// Check prometheus
 	response, err := http.Get("http://localhost:9090/api/v1/targets")
 	assert.NoError(t, err)

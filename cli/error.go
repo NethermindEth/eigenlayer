e8e48edc007f53e53d04e7aea1598b220e3d55c2
adriantpaez <adriantpaez@gmail.com>
2023-07-04 11:40:00 +0000

    feat: add non-prompt mode to the install command

--- cli/error.go
+++ cli/error.go
@@ -2,4 +2,8 @@ package cli
 
 import "errors"
 
-var ErrInvalidURL = errors.New("invalid URL")
+var (
+	ErrInvalidURL           = errors.New("invalid URL")
+	ErrOptionWithoutDefault = errors.New("option without default value")
+	ErrInvalidNumberOfArgs  = errors.New("invalid number of arguments")
+)
--- cli/install_test.go
+++ cli/install_test.go
@@ -22,12 +22,12 @@ func TestInstall(t *testing.T) {
 		{
 			name: "no arguments",
 			args: []string{},
-			err:  errors.New("accepts 1 arg(s), received 0"),
+			err:  fmt.Errorf("%w: accepts 1 arg, received 0", ErrInvalidNumberOfArgs),
 		},
 		{
 			name: "more than one argument",
 			args: []string{"arg1", "arg2"},
-			err:  errors.New("accepts 1 arg(s), received 2"),
+			err:  fmt.Errorf("%w: accepts 1 arg, received 2", ErrInvalidNumberOfArgs),
 		},
 		{
 			name: "invalid URL",
@@ -40,9 +40,9 @@ func TestInstall(t *testing.T) {
 			err:  nil,
 			daemonMock: func(d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
 				option := daemonMock.NewMockOption(gomock.NewController(t))
-				option.EXPECT().Name().Return("option1")
-				option.EXPECT().Default().Return("default1")
-				option.EXPECT().Help().Return("help1")
+				option.EXPECT().Name().Return("option1").Times(3)
+				option.EXPECT().Default().Return("default1").Times(2)
+				option.EXPECT().Help().Return("help1").Times(2)
 
 				gomock.InOrder(
 					d.EXPECT().
@@ -74,9 +74,9 @@ func TestInstall(t *testing.T) {
 			err:  nil,
 			daemonMock: func(d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
 				option := daemonMock.NewMockOption(gomock.NewController(t))
-				option.EXPECT().Name().Return("option1")
-				option.EXPECT().Default().Return("default1")
-				option.EXPECT().Help().Return("help1")
+				option.EXPECT().Name().Return("option1").Times(3)
+				option.EXPECT().Default().Return("default1").Times(2)
+				option.EXPECT().Help().Return("help1").Times(2)
 
 				gomock.InOrder(
 					d.EXPECT().
@@ -107,9 +107,9 @@ func TestInstall(t *testing.T) {
 			err:  errors.New("input string error"),
 			daemonMock: func(d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
 				option := daemonMock.NewMockOption(gomock.NewController(t))
-				option.EXPECT().Name().Return("option1")
-				option.EXPECT().Default().Return("default1")
-				option.EXPECT().Help().Return("help1")
+				option.EXPECT().Name().Return("option1").Times(3)
+				option.EXPECT().Default().Return("default1").Times(2)
+				option.EXPECT().Help().Return("help1").Times(2)
 
 				gomock.InOrder(
 					d.EXPECT().
@@ -177,9 +177,9 @@ func TestInstall(t *testing.T) {
 			err:  errors.New("install error"),
 			daemonMock: func(d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
 				option := daemonMock.NewMockOption(gomock.NewController(t))
-				option.EXPECT().Name().Return("option1")
-				option.EXPECT().Default().Return("default1")
-				option.EXPECT().Help().Return("help1")
+				option.EXPECT().Name().Return("option1").Times(3)
+				option.EXPECT().Default().Return("default1").Times(2)
+				option.EXPECT().Help().Return("help1").Times(2)
 
 				gomock.InOrder(
 					d.EXPECT().
@@ -209,9 +209,9 @@ func TestInstall(t *testing.T) {
 			err:  errors.New("confirm error"),
 			daemonMock: func(d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
 				option := daemonMock.NewMockOption(gomock.NewController(t))
-				option.EXPECT().Name().Return("option1")
-				option.EXPECT().Default().Return("default1")
-				option.EXPECT().Help().Return("help1")
+				option.EXPECT().Name().Return("option1").Times(3)
+				option.EXPECT().Default().Return("default1").Times(2)
+				option.EXPECT().Help().Return("help1").Times(2)
 
 				gomock.InOrder(
 					d.EXPECT().
@@ -233,6 +233,147 @@ func TestInstall(t *testing.T) {
 							Tag:     "default",
 						}).Return("mock-avs-default", nil),
 					p.EXPECT().Confirm("Run the new instance now?").Return(false, errors.New("confirm error")),
+				)
+			},
+		},
+		{
+			name: "--run flag",
+			args: []string{"--run", "https://github.com/NethermindEth/mock-avs"},
+			err:  nil,
+			daemonMock: func(d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				option := daemonMock.NewMockOption(gomock.NewController(t))
+				option.EXPECT().Name().Return("option1").Times(3)
+				option.EXPECT().Default().Return("default1").Times(2)
+				option.EXPECT().Help().Return("help1").Times(2)
+
+				gomock.InOrder(
+					d.EXPECT().
+						Pull("https://github.com/NethermindEth/mock-avs", "", true).
+						Return(daemon.PullResult{
+							Version: "v2.0.2",
+							Options: map[string][]daemon.Option{
+								"profile1": {option},
+							},
+						}, nil),
+					p.EXPECT().Select("Select a profile", []string{"profile1"}).Return("profile1", nil),
+					p.EXPECT().InputString("option1", "default1", "help1", gomock.Any()).Return("value1", nil),
+					d.EXPECT().
+						Install(daemon.InstallOptions{
+							URL:     "https://github.com/NethermindEth/mock-avs",
+							Version: "v2.0.2",
+							Profile: "profile1",
+							Options: []daemon.Option{option},
+							Tag:     "default",
+						}).Return("mock-avs-default", nil),
+					d.EXPECT().Run("mock-avs-default").Return(nil),
+				)
+			},
+		},
+		{
+			name: "--no-prompt and --run",
+			args: []string{"--no-prompt", "--run", "--profile", "profile1", "https://github.com/NethermindEth/mock-avs"},
+			err:  nil,
+			daemonMock: func(d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				option := daemonMock.NewMockOption(gomock.NewController(t))
+				option.EXPECT().Name().Return("option1").Times(2)
+				option.EXPECT().Default().Return("default1")
+				option.EXPECT().Help().Return("help1")
+				option.EXPECT().Set("default1").Return(nil)
+
+				gomock.InOrder(
+					d.EXPECT().
+						Pull("https://github.com/NethermindEth/mock-avs", "", true).
+						Return(daemon.PullResult{
+							Version: "v2.0.2",
+							Options: map[string][]daemon.Option{
+								"profile1": {option},
+							},
+						}, nil),
+					d.EXPECT().
+						Install(daemon.InstallOptions{
+							URL:     "https://github.com/NethermindEth/mock-avs",
+							Version: "v2.0.2",
+							Profile: "profile1",
+							Options: []daemon.Option{option},
+							Tag:     "default",
+						}).Return("mock-avs-default", nil),
+					d.EXPECT().Run("mock-avs-default").Return(nil),
+				)
+			},
+		},
+		{
+			name: "--no-prompt and --run false by default",
+			args: []string{"--no-prompt", "--profile", "profile1", "https://github.com/NethermindEth/mock-avs"},
+			err:  nil,
+			daemonMock: func(d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				option := daemonMock.NewMockOption(gomock.NewController(t))
+				option.EXPECT().Name().Return("option1").Times(2)
+				option.EXPECT().Default().Return("default1")
+				option.EXPECT().Help().Return("help1")
+				option.EXPECT().Set("default1").Return(nil)
+
+				gomock.InOrder(
+					d.EXPECT().
+						Pull("https://github.com/NethermindEth/mock-avs", "", true).
+						Return(daemon.PullResult{
+							Version: "v2.0.2",
+							Options: map[string][]daemon.Option{
+								"profile1": {option},
+							},
+						}, nil),
+					d.EXPECT().
+						Install(daemon.InstallOptions{
+							URL:     "https://github.com/NethermindEth/mock-avs",
+							Version: "v2.0.2",
+							Profile: "profile1",
+							Options: []daemon.Option{option},
+							Tag:     "default",
+						}).Return("mock-avs-default", nil),
+				)
+			},
+		},
+		{
+			name: "--no-prompt with an option without default value",
+			args: []string{"--profile=profile1", "--no-prompt", "--run=false", "https://github.com/NethermindEth/mock-avs"},
+			err:  fmt.Errorf("%w: %s", ErrOptionWithoutDefault, "option1"),
+			daemonMock: func(d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				option := daemonMock.NewMockOption(gomock.NewController(t))
+				option.EXPECT().Name().Return("option1").Times(3)
+				option.EXPECT().Help().Return("help1")
+				option.EXPECT().Default().Return("")
+
+				gomock.InOrder(
+					d.EXPECT().
+						Pull("https://github.com/NethermindEth/mock-avs", "", true).
+						Return(daemon.PullResult{
+							Version: "v2.0.2",
+							Options: map[string][]daemon.Option{
+								"profile1": {option},
+							},
+						}, nil),
+				)
+			},
+		},
+		{
+			name: "--no-prompt and set option error",
+			args: []string{"--profile=profile1", "--no-prompt", "https://github.com/NethermindEth/mock-avs", "--option.option1", "value1"},
+			err:  errors.New("error setting option"),
+			daemonMock: func(d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				option := daemonMock.NewMockOption(gomock.NewController(t))
+				option.EXPECT().Name().Return("option1").Times(2)
+				option.EXPECT().Help().Return("help1")
+				option.EXPECT().Default().Return("")
+				option.EXPECT().Set("value1").Return(errors.New("error setting option"))
+
+				gomock.InOrder(
+					d.EXPECT().
+						Pull("https://github.com/NethermindEth/mock-avs", "", true).
+						Return(daemon.PullResult{
+							Version: "v2.0.2",
+							Options: map[string][]daemon.Option{
+								"profile1": {option},
+							},
+						}, nil),
 				)
 			},
 		},

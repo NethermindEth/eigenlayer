d693f06b5370f915224dae22a4a28d268ea9e3bf
adriantpaez <adriantpaez@gmail.com>
2023-07-25 13:13:27 +0000

    feat: add --host flag to the plugin command

--- cli/plugin.go
+++ cli/plugin.go
@@ -11,9 +11,11 @@ func PluginCmd(d daemon.Daemon) *cobra.Command {
 	var (
 		instanceId     string
 		noDestroyImage bool
+		host           bool
+		pluginArgs     []string
 	)
 	cmd := cobra.Command{
-		Use:   "plugin [FLAGS] [INSTANCE_ID] [PLUGIN ARGS]",
+		Use:   "plugin [flags] [instance_id] [plugin_args]",
 		Short: "Run an AVS node plugin",
 		Long:  `Run a plugin. The instance id is required as the unique argument. The plugin arguments are passed to the plugin as is.`,
 		Args:  cobra.MinimumNArgs(1),
@@ -22,15 +24,21 @@ func PluginCmd(d daemon.Daemon) *cobra.Command {
 			if !d.HasInstance(instanceId) {
 				return errors.New("instance not found")
 			}
+			if len(args) > 1 {
+				pluginArgs = args[1:]
+			}
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return d.RunPlugin(instanceId, args[1:], noDestroyImage)
+			return d.RunPlugin(instanceId, pluginArgs, daemon.RunPluginOptions{
+				NoDestroyImage: noDestroyImage,
+				HostNetwork:    host,
+			})
 		},
 	}
 
 	cmd.Flags().BoolVar(&noDestroyImage, "no-rm-image", false, "Do not remove the plugin image after plugin execution")
-	cmd.DisableFlagParsing = true // Flag parsing is disable to support dynamic flags for plugin arguments
-
+	cmd.Flags().BoolVar(&host, "host", false, "Run the plugin on the host network instead of the AVS network")
+	cmd.Flags().SetInterspersed(false)
 	return &cmd
 }

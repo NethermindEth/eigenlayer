406aaded3b01c5f3f164e6fb28ae5c12a8afbcc5
AntiD2ta <mtpotro41@gmail.com>
2023-08-09 11:54:49 +0200

    ci(release): Add debian packaging job

---
+++ scripts/copy-ppa-packages-to-new-series.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+echo 'Installing Ubuntu Archive Tools'
+git clone https://git.launchpad.net/ubuntu-archive-tools
+sudo apt-get install ubuntu-dev-tools -y
+cd ubuntu-archive-tools
+echo 'Copying Packages'
+python3 copy-package -y -b -p nethermindeth --ppa-name=eigenlayer -s jammy --to-suite=focal eigenlayer
+python3 copy-package -y -b -p nethermindeth --ppa-name=eigenlayer -s jammy --to-suite=kinetic eigenlayer
+python3 copy-package -y -b -p nethermindeth --ppa-name=eigenlayer -s jammy --to-suite=bionic eigenlayer
+cd ..
+echo 'Cleanup'
+sudo rm -rf ubuntu-archive-tools

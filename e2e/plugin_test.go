856f24ae1de7d744bf5afcf2e112db445f3d3453
adriantpaez <adriantpaez@gmail.com>
2023-08-16 09:59:25 +0000

    test: update plugin e2e test cases

--- e2e/plugin_test.go
+++ e2e/plugin_test.go
@@ -16,7 +16,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-func TestPlugin_Install_RemoteContext(t *testing.T) {
+func TestPlugin_LocalInstall_RemoteContext(t *testing.T) {
 	// Test context
 	var (
 		testDir    = t.TempDir()
@@ -57,7 +57,64 @@ func TestPlugin_Install_RemoteContext(t *testing.T) {
 	e2eTest.run()
 }
 
-func TestPlugin_Install_LocalContext(t *testing.T) {
+func TestPlugin_LocalInstall_RemoteContext_Run(t *testing.T) {
+	// Test context
+	var (
+		testDir      = t.TempDir()
+		pkgDir       = filepath.Join(testDir, "mock-avs")
+		runPluginErr error
+		eventsSince  time.Time
+		eventsUntil  time.Time
+	)
+	e2eTest := newE2ETestCase(
+		t,
+		// Arrange
+		func(t *testing.T, egnPath string) error {
+			err := os.MkdirAll(pkgDir, 0o755)
+			if err != nil {
+				return err
+			}
+			err = runCommand(t, "git", "clone", "--single-branch", "-b", latestMockAVSVersion, mockAVSRepo, pkgDir)
+			if err != nil {
+				return err
+			}
+			// remove .git folder
+			if err := os.RemoveAll(filepath.Join(pkgDir, ".git")); err != nil {
+				return err
+			}
+			return runCommand(t, egnPath, "local-install", pkgDir, "--profile", "option-returner", "--run")
+		},
+		// Act
+		func(t *testing.T, egnPath string) {
+			eventsSince = time.Now()
+			runPluginErr = runCommand(t, egnPath, "plugin", "mock-avs-default")
+			eventsUntil = time.Now()
+		},
+		// Assert
+		func(t *testing.T) {
+			assert.NoError(t, runPluginErr, "plugin command should succeed")
+
+			// Check docker events
+			pluginContainerID := ""
+			networkID, err := getNetworkIDByName("eigenlayer")
+			require.NoError(t, err, "getNetworkIDByName should succeed")
+			events, err := docker.EventsRange(context.Background(), eventsSince, eventsUntil)
+			require.NoError(t, err, "docker events should succeed")
+
+			events.CheckInOrder(t,
+				docker.NewImageTagged("mock-avs-default", "latest"),
+				docker.NewContainerCreated("eigen-plugin-mock-avs-default", &pluginContainerID),
+				docker.NewNetworkConnect(&pluginContainerID, &networkID),
+				docker.NewNetworkDisconnect(&pluginContainerID, &networkID),
+				docker.NewContainerDies(&pluginContainerID),
+				docker.NewContainerDestroy(&pluginContainerID),
+			)
+		},
+	)
+	e2eTest.run()
+}
+
+func TestPlugin_LocalInstall_LocalContext(t *testing.T) {
 	// Test context
 	var (
 		testDir    = t.TempDir()
@@ -135,7 +192,94 @@ func TestPlugin_Install_LocalContext(t *testing.T) {
 	e2eTest.run()
 }
 
-func TestPlugin_Install_RemoteImage(t *testing.T) {
+func TestPlugin_LocalInstall_LocalContext_Run(t *testing.T) {
+	// Test context
+	var (
+		testDir      = t.TempDir()
+		pkgDir       = filepath.Join(testDir, "mock-avs")
+		runPluginErr error
+		eventsSince  time.Time
+		eventsUntil  time.Time
+	)
+	e2eTest := newE2ETestCase(
+		t,
+		// Arrange
+		func(t *testing.T, egnPath string) error {
+			if err := os.MkdirAll(pkgDir, 0o755); err != nil {
+				return err
+			}
+			err := runCommand(t, "git", "clone", "--single-branch", "-b", latestMockAVSVersion, mockAVSRepo, pkgDir)
+			if err != nil {
+				return err
+			}
+			// remove .git folder
+			if err := os.RemoveAll(filepath.Join(pkgDir, ".git")); err != nil {
+				return err
+			}
+			// modify manifest to use local context
+			manifestFile, err := os.OpenFile(filepath.Join(pkgDir, "pkg", "manifest.yml"), os.O_RDWR, 0o755)
+			if err != nil {
+				return err
+			}
+			manifestData, err := io.ReadAll(manifestFile)
+			if err != nil {
+				return err
+			}
+			if err = manifestFile.Close(); err != nil {
+				return err
+			}
+			var manifest package_handler.Manifest
+			if err = yaml.Unmarshal(manifestData, &manifest); err != nil {
+				return err
+			}
+			manifest.Plugin.BuildFrom = "../plugin"
+			manifestData, err = yaml.Marshal(manifest)
+			if err != nil {
+				return err
+			}
+			manifestFile, err = os.OpenFile(filepath.Join(pkgDir, "pkg", "manifest.yml"), os.O_RDWR|os.O_TRUNC, 0o755)
+			if err != nil {
+				return err
+			}
+			if _, err = manifestFile.Write(manifestData); err != nil {
+				return err
+			}
+			if err = manifestFile.Close(); err != nil {
+				return err
+			}
+			return runCommand(t, egnPath, "local-install", pkgDir, "--profile", "option-returner", "--run")
+		},
+		// Act
+		func(t *testing.T, egnPath string) {
+			eventsSince = time.Now()
+			runPluginErr = runCommand(t, egnPath, "plugin", "mock-avs-default")
+			eventsUntil = time.Now()
+		},
+		// Assert
+		func(t *testing.T) {
+			assert.NoError(t, runPluginErr, "plugin command should succeed")
+
+			// Check docker events
+			pluginContainerID := ""
+			networkID, err := getNetworkIDByName("eigenlayer")
+			require.NoError(t, err, "getNetworkIDByName should succeed")
+			events, err := docker.EventsRange(context.Background(), eventsSince, eventsUntil)
+			require.NoError(t, err, "docker events should succeed")
+
+			events.CheckInOrder(t,
+				docker.NewImageTagged("mock-avs-default", "latest"),
+				docker.NewContainerCreated("eigen-plugin-mock-avs-default", &pluginContainerID),
+				docker.NewNetworkConnect(&pluginContainerID, &networkID),
+				docker.NewNetworkDisconnect(&pluginContainerID, &networkID),
+				docker.NewContainerDies(&pluginContainerID),
+				docker.NewContainerDestroy(&pluginContainerID),
+			)
+		},
+	)
+	e2eTest.run()
+}
+
+func TestPlugin_LocalInstall_RemoteImage(t *testing.T) {
 	// Test context
 	var (
 		testDir    = t.TempDir()
@@ -207,11 +351,37 @@ func TestPlugin_Install_RemoteImage(t *testing.T) {
 	e2eTest.run()
 }
 
-func TestPlugin_Run_RemoteContext(t *testing.T) {
+func TestPlugin_Install_RemoteContext(t *testing.T) {
 	// Test context
 	var (
-		testDir      = t.TempDir()
-		pkgDir       = filepath.Join(testDir, "mock-avs")
+		installErr error
+	)
+	e2eTest := newE2ETestCase(
+		t,
+		// Arrange
+		nil,
+		// Act
+		func(t *testing.T, egnPath string) {
+			installErr = runCommand(t, egnPath, "install", "--profile", "option-returner", "--no-prompt", "--yes", "--version", latestMockAVSVersion, "https://github.com/NethermindEth/mock-avs")
+		},
+		// Assert
+		func(t *testing.T) {
+			assert.NoError(t, installErr, "local-install command should succeed")
+			instanceID := "mock-avs-default"
+			checkInstanceExists(t, instanceID)
+			instance, err := getInstance(t, instanceID)
+			require.NoError(t, err, "getInstance should succeed")
+			assert.NotNil(t, instance.Plugin, "plugin should be installed")
+			assert.Equal(t, instance.Plugin.Type, data.PluginTypeRemoteContext, "plugin type should be ", data.PluginTypeRemoteContext)
+			assert.Equal(t, instance.Plugin.Src, "https://github.com/NethermindEth/mock-avs.git#main:plugin")
+		},
+	)
+	e2eTest.run()
+}
+
+func TestPlugin_Install_RemoteContext_Run(t *testing.T) {
+	// Test context
+	var (
 		installErr   error
 		runPluginErr error
 		eventsSince  time.Time
@@ -221,19 +391,7 @@ func TestPlugin_Run_RemoteContext(t *testing.T) {
 		t,
 		// Arrange
 		func(t *testing.T, egnPath string) error {
-			err := os.MkdirAll(pkgDir, 0o755)
-			if err != nil {
-				return err
-			}
-			err = runCommand(t, "git", "clone", "--single-branch", "-b", latestMockAVSVersion, mockAVSRepo, pkgDir)
-			if err != nil {
-				return err
-			}
-			// remove .git folder
-			if err := os.RemoveAll(filepath.Join(pkgDir, ".git")); err != nil {
-				return err
-			}
-			return runCommand(t, egnPath, "local-install", pkgDir, "--profile", "option-returner", "--run")
+			return runCommand(t, egnPath, "install", "--profile", "option-returner", "--no-prompt", "--yes", "--version", latestMockAVSVersion, "https://github.com/NethermindEth/mock-avs")
 		},
 		// Act
 		func(t *testing.T, egnPath string) {
@@ -243,7 +401,7 @@ func TestPlugin_Run_RemoteContext(t *testing.T) {
 		},
 		// Assert
 		func(t *testing.T) {
-			assert.NoError(t, installErr, "local-install command should succeed")
+			assert.NoError(t, installErr, "install command should succeed")
 			assert.NoError(t, runPluginErr, "plugin command should succeed")
 
 			// Check docker events

42432ec77f87c02aacf9a8fab07bd6874c648f73
adriantpaez <adriantpaez@gmail.com>
2023-08-17 11:00:33 +0000

    test: add e2e test cases

---
+++ e2e/stop_test.go
@@ -0,0 +1,56 @@
+package e2e
+
+import (
+	"testing"
+	"time"
+
+	"github.com/NethermindEth/eigenlayer/e2e/docker"
+	"github.com/stretchr/testify/require"
+)
+
+// Test_Stop checks that the stop command stops all the container of the mock-avs
+// option-returner profile without error.
+func Test_Stop(t *testing.T) {
+	// Test context
+	var (
+		stopErr error
+	)
+	e2eTest := newE2ETestCase(t,
+		func(t *testing.T, egnPath string) error {
+			err := runCommand(t, egnPath, "install", "--profile", "option-returner", "--no-prompt", "--yes", "--version", latestMockAVSVersion, "https://github.com/NethermindEth/mock-avs")
+			if err != nil {
+				return err
+			}
+			err = runCommand(t, egnPath, "run", "mock-avs-default")
+			if err != nil {
+				return err
+			}
+			return docker.WaitUntilRunning("option-returner", 10*time.Second)
+		},
+		func(t *testing.T, egnPath string) {
+			stopErr = runCommand(t, egnPath, "stop", "mock-avs-default")
+		},
+		func(t *testing.T) {
+			require.NoError(t, stopErr, "stop command should succeed")
+			checkContainerNotRunning(t, "option-returner")
+		})
+	e2eTest.run()
+}
+
+// Test_Stop_NonExistingInstance checks that the stop command fails when the
+// instance does not exist.
+func Test_Stop_NonExistingInstance(t *testing.T) {
+	// Test context
+	var (
+		stopErr error
+	)
+	e2eTest := newE2ETestCase(t,
+		nil,
+		func(t *testing.T, egnPath string) {
+			stopErr = runCommand(t, egnPath, "stop", "mock-avs-default")
+		},
+		func(t *testing.T) {
+			require.Error(t, stopErr, "stop command should fail")
+		})
+	e2eTest.run()
+}

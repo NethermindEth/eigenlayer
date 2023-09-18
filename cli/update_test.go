5da0ba69080db437430f6869a4d65d6847bb62f4
adriantpaez <adriantpaez@gmail.com>
2023-09-18 09:54:53 +0000

    test: update command

---
+++ cli/update_test.go
@@ -0,0 +1,217 @@
+package cli
+
+import (
+	"fmt"
+	"testing"
+
+	daemonMock "github.com/NethermindEth/eigenlayer/cli/mocks"
+	prompterMock "github.com/NethermindEth/eigenlayer/cli/prompter/mocks"
+	"github.com/NethermindEth/eigenlayer/internal/common"
+	"github.com/NethermindEth/eigenlayer/pkg/daemon"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpdate(t *testing.T) {
+	instanceId := "mock-avs-default"
+	tc := []struct {
+		name   string
+		args   []string
+		mocker func(ctrl *gomock.Controller, d *daemonMock.MockDaemon, p *prompterMock.MockPrompter)
+		err    error
+	}{
+		{
+			name: "update to latest version",
+			args: []string{instanceId},
+			mocker: func(ctrl *gomock.Controller, d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				oldOption := daemonMock.NewMockOption(ctrl)
+				newOption := daemonMock.NewMockOption(ctrl)
+				mergedOption := daemonMock.NewMockOption(ctrl)
+
+				oldOption.EXPECT().IsSet().Return(true)
+				oldOption.EXPECT().Value().Return("old-value", nil)
+				oldOption.EXPECT().Name().Return("old-option").Times(3)
+				mergedOption.EXPECT().IsSet().Return(true).Times(2)
+				mergedOption.EXPECT().Value().Return("old-value", nil)
+				mergedOption.EXPECT().Name().Return("old-option")
+
+				gomock.InOrder(
+					d.EXPECT().PullUpdate(instanceId, daemon.PullTarget{}).Return(daemon.PullUpdateResult{
+						Name:          "mock-avs",
+						Tag:           "default",
+						Url:           common.MockAvsPkg.Repo(),
+						Profile:       "option-returner",
+						OldVersion:    "v5.4.0",
+						NewVersion:    common.MockAvsPkg.Version(),
+						OldCommit:     "b64c50c15e53ae7afebbdbe210b834d1ee471043",
+						NewCommit:     common.MockAvsPkg.CommitHash(),
+						HasPlugin:     true,
+						OldOptions:    []daemon.Option{oldOption},
+						NewOptions:    []daemon.Option{newOption},
+						MergedOptions: []daemon.Option{mergedOption},
+						HardwareRequirements: daemon.HardwareRequirements{
+							MinCPUCores:                 2,
+							MinRAM:                      2048,
+							MinFreeSpace:                5120,
+							StopIfRequirementsAreNotMet: true,
+						},
+					}, nil),
+					d.EXPECT().Uninstall(instanceId).Return(nil),
+					d.EXPECT().Install(daemon.InstallOptions{
+						Name:    "mock-avs",
+						Tag:     "default",
+						URL:     common.MockAvsPkg.Repo(),
+						Profile: "option-returner",
+						Version: common.MockAvsPkg.Version(),
+						Commit:  common.MockAvsPkg.CommitHash(),
+						Options: []daemon.Option{mergedOption},
+					}).Return(instanceId, nil),
+					p.EXPECT().Confirm("Run the new instance now?").Return(true, nil),
+					d.EXPECT().Run(instanceId),
+				)
+			},
+		},
+		{
+			name: "update to fixed version",
+			args: []string{instanceId, common.MockAvsPkg.Version()},
+			mocker: func(ctrl *gomock.Controller, d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				oldOption := daemonMock.NewMockOption(ctrl)
+				newOption := daemonMock.NewMockOption(ctrl)
+				mergedOption := daemonMock.NewMockOption(ctrl)
+
+				oldOption.EXPECT().IsSet().Return(true)
+				oldOption.EXPECT().Value().Return("old-value", nil)
+				oldOption.EXPECT().Name().Return("old-option").Times(3)
+				mergedOption.EXPECT().IsSet().Return(true).Times(2)
+				mergedOption.EXPECT().Value().Return("old-value", nil)
+				mergedOption.EXPECT().Name().Return("old-option")
+
+				gomock.InOrder(
+					d.EXPECT().PullUpdate(instanceId, daemon.PullTarget{
+						Version: common.MockAvsPkg.Version(),
+					}).Return(daemon.PullUpdateResult{
+						Name:          "mock-avs",
+						Tag:           "default",
+						Url:           common.MockAvsPkg.Repo(),
+						Profile:       "option-returner",
+						OldVersion:    "v5.4.0",
+						NewVersion:    common.MockAvsPkg.Version(),
+						OldCommit:     "b64c50c15e53ae7afebbdbe210b834d1ee471043",
+						NewCommit:     common.MockAvsPkg.CommitHash(),
+						HasPlugin:     true,
+						OldOptions:    []daemon.Option{oldOption},
+						NewOptions:    []daemon.Option{newOption},
+						MergedOptions: []daemon.Option{mergedOption},
+						HardwareRequirements: daemon.HardwareRequirements{
+							MinCPUCores:                 2,
+							MinRAM:                      2048,
+							MinFreeSpace:                5120,
+							StopIfRequirementsAreNotMet: true,
+						},
+					}, nil),
+					d.EXPECT().Uninstall(instanceId).Return(nil),
+					d.EXPECT().Install(daemon.InstallOptions{
+						Name:    "mock-avs",
+						Tag:     "default",
+						URL:     common.MockAvsPkg.Repo(),
+						Profile: "option-returner",
+						Version: common.MockAvsPkg.Version(),
+						Commit:  common.MockAvsPkg.CommitHash(),
+						Options: []daemon.Option{mergedOption},
+					}).Return(instanceId, nil),
+					p.EXPECT().Confirm("Run the new instance now?").Return(true, nil),
+					d.EXPECT().Run(instanceId),
+				)
+			},
+		},
+		{
+			name: "update to fixed commit",
+			args: []string{instanceId, common.MockAvsPkg.CommitHash()},
+			mocker: func(ctrl *gomock.Controller, d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				oldOption := daemonMock.NewMockOption(ctrl)
+				newOption := daemonMock.NewMockOption(ctrl)
+				mergedOption := daemonMock.NewMockOption(ctrl)
+
+				oldOption.EXPECT().IsSet().Return(true)
+				oldOption.EXPECT().Value().Return("old-value", nil)
+				oldOption.EXPECT().Name().Return("old-option").Times(3)
+				mergedOption.EXPECT().IsSet().Return(true).Times(2)
+				mergedOption.EXPECT().Value().Return("old-value", nil)
+				mergedOption.EXPECT().Name().Return("old-option")
+
+				gomock.InOrder(
+					d.EXPECT().PullUpdate(instanceId, daemon.PullTarget{
+						Commit: common.MockAvsPkg.CommitHash(),
+					}).Return(daemon.PullUpdateResult{
+						Name:          "mock-avs",
+						Tag:           "default",
+						Url:           common.MockAvsPkg.Repo(),
+						Profile:       "option-returner",
+						OldVersion:    "v5.4.0",
+						NewVersion:    common.MockAvsPkg.Version(),
+						OldCommit:     "b64c50c15e53ae7afebbdbe210b834d1ee471043",
+						NewCommit:     common.MockAvsPkg.CommitHash(),
+						HasPlugin:     true,
+						OldOptions:    []daemon.Option{oldOption},
+						NewOptions:    []daemon.Option{newOption},
+						MergedOptions: []daemon.Option{mergedOption},
+						HardwareRequirements: daemon.HardwareRequirements{
+							MinCPUCores:                 2,
+							MinRAM:                      2048,
+							MinFreeSpace:                5120,
+							StopIfRequirementsAreNotMet: true,
+						},
+					}, nil),
+					d.EXPECT().Uninstall(instanceId).Return(nil),
+					d.EXPECT().Install(daemon.InstallOptions{
+						Name:    "mock-avs",
+						Tag:     "default",
+						URL:     common.MockAvsPkg.Repo(),
+						Profile: "option-returner",
+						Version: common.MockAvsPkg.Version(),
+						Commit:  common.MockAvsPkg.CommitHash(),
+						Options: []daemon.Option{mergedOption},
+					}).Return(instanceId, nil),
+					p.EXPECT().Confirm("Run the new instance now?").Return(true, nil),
+					d.EXPECT().Run(instanceId),
+				)
+			},
+		},
+		{
+			name: "invalid arguments, instance id is required",
+			args: []string{},
+			err:  fmt.Errorf("%w: instance-id is required", ErrInvalidNumberOfArgs),
+		},
+		{
+			name: "invalid arguments, more than 2 arguments",
+			args: []string{"instance-id", "version", "extra-arg"},
+			err:  fmt.Errorf("%w: too many arguments", ErrInvalidNumberOfArgs),
+		},
+		{
+			name: "invalid arguments, invalid version or commit",
+			args: []string{"instance-id", "invalid-version"},
+			err:  fmt.Errorf("%w: invalid version or commit", ErrInvalidArgs),
+		},
+	}
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			d := daemonMock.NewMockDaemon(ctrl)
+			p := prompterMock.NewMockPrompter(ctrl)
+			if tt.mocker != nil {
+				tt.mocker(ctrl, d, p)
+			}
+
+			updateCmd := UpdateCmd(d, p)
+
+			updateCmd.SetArgs(tt.args)
+			err := updateCmd.Execute()
+
+			if tt.err == nil {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, err, tt.err.Error())
+			}
+		})
+	}
+}

4bda1cbb37f38da70d42b2604e7310740604070d
AntiD2ta <mtpotro41@gmail.com>
2023-09-27 12:09:20 +0000

    feat(local-ins): Use empty str for default-less options

--- e2e/local_install_test.go
+++ e2e/local_install_test.go
@@ -446,3 +446,79 @@ func TestLocalInstall_ProfileWithHiddenOptionsNotSet(t *testing.T) {
 	// Run test case
 	e2eTest.run()
 }
+
+const newOptions = `
+  - name: "hidden-option-test"
+    target: HIDDEN_WITH_DEFAULT_TEST
+    type: str
+    default: test
+    hidden: true
+    help: "Hidden option test"
+  - name: "option-without-default-test"
+    target: OPTION_WITHOUT_DEFAULT_TEST
+    type: str
+    validate:
+      re2_regex: "^eigen.*" # Words that start with eigen
+    help: "Option without default"
+`
+
+func TestLocalInstall_OptionsWithoutDefault(t *testing.T) {
+	// Test context
+	var (
+		testDir = t.TempDir()
+		pkgDir  = filepath.Join(testDir, "mock-avs")
+		runErr  error
+	)
+	// Build test case
+	e2eTest := newE2ETestCase(
+		t,
+		// Arrange
+		func(t *testing.T, egnPath string) error {
+			if err := buildMockAvsImages(t); err != nil {
+				return err
+			}
+			err := os.MkdirAll(pkgDir, 0o755)
+			if err != nil {
+				return err
+			}
+			err = runCommand(t, "git", "clone", "--single-branch", "-b", common.MockAvsPkg.Version(), common.MockAvsPkg.Repo(), pkgDir)
+			if err != nil {
+				return err
+			}
+			// Modify health-checker to add a hidden option will default and a non-hidden option without default.
+			// Both TARGETS should be the empty string in the .env.
+			f, err := os.OpenFile(filepath.Join(pkgDir, "pkg", "health-checker", "profile.yml"), os.O_APPEND|os.O_WRONLY, 0644)
+			if err != nil {
+				t.Fatalf("failed to open profile.yml: %v", err)
+			}
+			defer f.Close()
+
+			if _, err = f.WriteString(newOptions); err != nil {
+				t.Fatalf("failed to write to profile.yml: %v", err)
+			}
+
+			// data, _ := os.ReadFile(filepath.Join(pkgDir, "pkg", "health-checker", "profile.yml"))
+			// t.Logf("profile.yml: \n %s \n", data)
+			// t.Fatal("stop")
+
+			// remove .git folder
+			return os.RemoveAll(filepath.Join(pkgDir, ".git"))
+		},
+		// Act
+		func(t *testing.T, egnPath string) {
+			runErr = runCommand(t, egnPath, "local-install", pkgDir, "--profile", "health-checker", "--run", "--log-debug")
+		},
+		// Assert
+		func(t *testing.T) {
+			assert.NoError(t, runErr, "local-install command should succeed")
+			checkInstanceInstalled(t, "mock-avs-default")
+			checkContainerRunning(t, "health-checker")
+			healthCheckerIP, err := getContainerIPByName("health-checker", "eigenlayer")
+			require.NoError(t, err, "failed to get health-checker container IP")
+			checkAVSHealth(t, healthCheckerIP, "8090", 200)
+			checkEnvTargets(t, "mock-avs-default", "HIDDEN_WITH_DEFAULT_TEST", "OPTION_WITHOUT_DEFAULT_TEST")
+		},
+	)
+	// Run test case
+	e2eTest.run()
+}

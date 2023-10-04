484afaf1b8f975b1866f0eebeac5304e90f6fe1d
Madhur Shrimal <madhur.shrimal@gmail.com>
2023-10-04 01:32:37 -0700

    feat: Implement onchain code features (#85)
    * init op register feature
    
    operator registration stuff
    
    more options to register
    
    * password commit
    
    * update with latest interface
    
    * add password for ops and update sdk
    
    * update to remove eigensdk
    
    * make format
    
    * add chainid as param
    
    * update command description
    
    * address comments
    
    * add more details about update op

---
+++ cli/operator/update.go
@@ -0,0 +1,127 @@
+package operator
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/NethermindEth/eigenlayer/cli/prompter"
+	"github.com/NethermindEth/eigenlayer/internal/types"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/spf13/cobra"
+
+	eigenChainio "github.com/Layr-Labs/eigensdk-go/chainio/clients"
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients/eth"
+	elContracts "github.com/Layr-Labs/eigensdk-go/chainio/elcontracts"
+	eigensdkLogger "github.com/Layr-Labs/eigensdk-go/logging"
+	"github.com/Layr-Labs/eigensdk-go/metrics"
+	eigensdkUtils "github.com/Layr-Labs/eigensdk-go/utils"
+)
+
+func UpdateCmd(p prompter.Prompter) *cobra.Command {
+	var (
+		configurationFilePath string
+		help                  bool
+		operatorCfg           types.OperatorConfig
+		signerTypeFlag        string
+		signerType            types.SignerType
+		privateKeyHex         string
+	)
+	cmd := cobra.Command{
+		Use:   "update [flags]",
+		Short: "Updates the operator metadata",
+		Long: `
+		Updates the operator metadata onchain which includes 
+			- metadata url
+			- delegation approver address
+			- earnings reciver address
+			- staker opt out window blocks
+
+		Requires the same file used for registration as a flag input to
+		--configuration-file
+		`,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			// Parse static flags
+			cmd.DisableFlagParsing = false
+			cmd.FParseErrWhitelist.UnknownFlags = true // Don't show error for unknown flags to allow dynamic flags
+			err := cmd.ParseFlags(args)
+			if err != nil {
+				return err
+			}
+
+			// Skip execution if help flag is set
+			help, err = cmd.Flags().GetBool("help")
+			if err != nil {
+				return err
+			}
+			if help {
+				return nil
+			}
+
+			err = eigensdkUtils.ReadYamlConfig(configurationFilePath, &operatorCfg)
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Operator configuration file read successfully %s\n", operatorCfg.Operator.Address)
+
+			signerType, err = validateSignerType(signerTypeFlag, operatorCfg)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			llog, err := eigensdkLogger.NewZapLogger(eigensdkLogger.Development)
+			if err != nil {
+				return err
+			}
+
+			localSigner, err := getSigner(p, signerType, privateKeyHex, operatorCfg)
+			if err != nil {
+				return err
+			}
+
+			ethClient, err := eth.NewClient(operatorCfg.EthRPCUrl)
+			if err != nil {
+				return err
+			}
+
+			elContractsClient, err := eigenChainio.NewELContractsChainClient(
+				common.HexToAddress(operatorCfg.ELSlasherAddress),
+				common.HexToAddress(operatorCfg.BlsPublicKeyCompendiumAddress),
+				ethClient,
+				ethClient,
+				llog,
+			)
+			if err != nil {
+				return err
+			}
+
+			noopMetrics := metrics.NewNoopMetrics()
+			elWriter := elContracts.NewELChainWriter(
+				elContractsClient,
+				ethClient,
+				localSigner,
+				llog,
+				noopMetrics,
+			)
+
+			if err != nil {
+				return err
+			}
+			_, err = elWriter.UpdateOperatorDetails(context.Background(), operatorCfg.Operator)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Operator updated successfully")
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&configurationFilePath, "configuration-file", "", "Path to the configuration file")
+	cmd.Flags().StringVar(&signerTypeFlag, "signer-type", "", "Signer type (private_key, local_keystore)")
+	cmd.Flags().StringVar(&privateKeyHex, "private-key-hex", "", "Private key hex (used only with private_key signer type)")
+
+	return &cmd
+}

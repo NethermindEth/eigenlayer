2a03a1f738dfbc1b9fb8486df0ee1f87525404f8
adriantpaez <adriantpaez@gmail.com>
2023-10-06 07:51:59 +0000

    feat: add restore to update cli

--- cli/update_test.go
+++ cli/update_test.go
@@ -283,6 +283,204 @@ func TestUpdate(t *testing.T) {
 			},
 		},
 		{
+			name: "update with backup, fails to uninstall current instance, restore backup successfully",
+			args: []string{instanceId, "--backup"},
+			err:  nil,
+			mocker: func(ctrl *gomock.Controller, d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				oldOption := daemonMock.NewMockOption(ctrl)
+				newOption := daemonMock.NewMockOption(ctrl)
+				mergedOption := daemonMock.NewMockOption(ctrl)
+
+				oldOption.EXPECT().IsSet().Return(true)
+				oldOption.EXPECT().Value().Return("old-value", nil)
+				oldOption.EXPECT().Name().Return("old-option").Times(3)
+				mergedOption.EXPECT().IsSet().Return(true).Times(2)
+				mergedOption.EXPECT().Value().Return("old-value", nil)
+				mergedOption.EXPECT().Name().Return("old-option").Times(2)
+				mergedOption.EXPECT().Default().Return("default-value")
+				mergedOption.EXPECT().Help().Return("option help")
+
+				gomock.InOrder(
+					d.EXPECT().PullUpdate(instanceId, daemon.PullTarget{}).Return(daemon.PullUpdateResult{
+						Name:          "mock-avs",
+						Tag:           "default",
+						Url:           common.MockAvsPkg.Repo(),
+						Profile:       "option-returner",
+						OldVersion:    "v5.4.0",
+						NewVersion:    common.MockAvsPkg.Version(),
+						OldCommit:     "b64c50c15e53ae7afebbdbe210b834d1ee471043",
+						NewCommit:     common.MockAvsPkg.CommitHash(),
+						HasPlugin:     true,
+						OldOptions:    []daemon.Option{oldOption},
+						NewOptions:    []daemon.Option{newOption},
+						MergedOptions: []daemon.Option{mergedOption},
+						HardwareRequirements: daemon.HardwareRequirements{
+							MinCPUCores:                 2,
+							MinRAM:                      2048,
+							MinFreeSpace:                5120,
+							StopIfRequirementsAreNotMet: true,
+						},
+					}, nil),
+					d.EXPECT().Backup(instanceId).Return(fmt.Sprintf("%s-%d", instanceId, time.Now().Unix()), nil),
+					d.EXPECT().Uninstall(instanceId).Return(assert.AnError),
+					d.EXPECT().Restore(gomock.Any(), false).Return(nil),
+				)
+			},
+		},
+		{
+			name: "update with backup, fails to uninstall current instance, restore backup fails",
+			args: []string{instanceId, "--backup"},
+			err:  assert.AnError,
+			mocker: func(ctrl *gomock.Controller, d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				oldOption := daemonMock.NewMockOption(ctrl)
+				newOption := daemonMock.NewMockOption(ctrl)
+				mergedOption := daemonMock.NewMockOption(ctrl)
+
+				oldOption.EXPECT().IsSet().Return(true)
+				oldOption.EXPECT().Value().Return("old-value", nil)
+				oldOption.EXPECT().Name().Return("old-option").Times(3)
+				mergedOption.EXPECT().IsSet().Return(true).Times(2)
+				mergedOption.EXPECT().Value().Return("old-value", nil)
+				mergedOption.EXPECT().Name().Return("old-option").Times(2)
+				mergedOption.EXPECT().Default().Return("default-value")
+				mergedOption.EXPECT().Help().Return("option help")
+
+				gomock.InOrder(
+					d.EXPECT().PullUpdate(instanceId, daemon.PullTarget{}).Return(daemon.PullUpdateResult{
+						Name:          "mock-avs",
+						Tag:           "default",
+						Url:           common.MockAvsPkg.Repo(),
+						Profile:       "option-returner",
+						OldVersion:    "v5.4.0",
+						NewVersion:    common.MockAvsPkg.Version(),
+						OldCommit:     "b64c50c15e53ae7afebbdbe210b834d1ee471043",
+						NewCommit:     common.MockAvsPkg.CommitHash(),
+						HasPlugin:     true,
+						OldOptions:    []daemon.Option{oldOption},
+						NewOptions:    []daemon.Option{newOption},
+						MergedOptions: []daemon.Option{mergedOption},
+						HardwareRequirements: daemon.HardwareRequirements{
+							MinCPUCores:                 2,
+							MinRAM:                      2048,
+							MinFreeSpace:                5120,
+							StopIfRequirementsAreNotMet: true,
+						},
+					}, nil),
+					d.EXPECT().Backup(instanceId).Return(fmt.Sprintf("%s-%d", instanceId, time.Now().Unix()), nil),
+					d.EXPECT().Uninstall(instanceId).Return(assert.AnError),
+					d.EXPECT().Restore(gomock.Any(), false).Return(assert.AnError),
+				)
+			},
+		},
+		{
+			name: "update with backup, fails to install new instance, restore backup successfully",
+			args: []string{instanceId, "--backup"},
+			err:  nil,
+			mocker: func(ctrl *gomock.Controller, d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				oldOption := daemonMock.NewMockOption(ctrl)
+				newOption := daemonMock.NewMockOption(ctrl)
+				mergedOption := daemonMock.NewMockOption(ctrl)
+
+				oldOption.EXPECT().IsSet().Return(true)
+				oldOption.EXPECT().Value().Return("old-value", nil)
+				oldOption.EXPECT().Name().Return("old-option").Times(3)
+				mergedOption.EXPECT().IsSet().Return(true).Times(2)
+				mergedOption.EXPECT().Value().Return("old-value", nil)
+				mergedOption.EXPECT().Name().Return("old-option").Times(2)
+				mergedOption.EXPECT().Default().Return("default-value")
+				mergedOption.EXPECT().Help().Return("option help")
+
+				gomock.InOrder(
+					d.EXPECT().PullUpdate(instanceId, daemon.PullTarget{}).Return(daemon.PullUpdateResult{
+						Name:          "mock-avs",
+						Tag:           "default",
+						Url:           common.MockAvsPkg.Repo(),
+						Profile:       "option-returner",
+						OldVersion:    "v5.4.0",
+						NewVersion:    common.MockAvsPkg.Version(),
+						OldCommit:     "b64c50c15e53ae7afebbdbe210b834d1ee471043",
+						NewCommit:     common.MockAvsPkg.CommitHash(),
+						HasPlugin:     true,
+						OldOptions:    []daemon.Option{oldOption},
+						NewOptions:    []daemon.Option{newOption},
+						MergedOptions: []daemon.Option{mergedOption},
+						HardwareRequirements: daemon.HardwareRequirements{
+							MinCPUCores:                 2,
+							MinRAM:                      2048,
+							MinFreeSpace:                5120,
+							StopIfRequirementsAreNotMet: true,
+						},
+					}, nil),
+					d.EXPECT().Backup(instanceId).Return(fmt.Sprintf("%s-%d", instanceId, time.Now().Unix()), nil),
+					d.EXPECT().Uninstall(instanceId).Return(nil),
+					d.EXPECT().Install(daemon.InstallOptions{
+						Name:    "mock-avs",
+						Tag:     "default",
+						URL:     common.MockAvsPkg.Repo(),
+						Profile: "option-returner",
+						Version: common.MockAvsPkg.Version(),
+						Commit:  common.MockAvsPkg.CommitHash(),
+						Options: []daemon.Option{mergedOption},
+					}).Return("", assert.AnError),
+					d.EXPECT().Restore(gomock.Any(), false).Return(nil),
+				)
+			},
+		},
+		{
+			name: "update with backup, fails to install new instance, restore backup fails",
+			args: []string{instanceId, "--backup"},
+			err:  assert.AnError,
+			mocker: func(ctrl *gomock.Controller, d *daemonMock.MockDaemon, p *prompterMock.MockPrompter) {
+				oldOption := daemonMock.NewMockOption(ctrl)
+				newOption := daemonMock.NewMockOption(ctrl)
+				mergedOption := daemonMock.NewMockOption(ctrl)
+
+				oldOption.EXPECT().IsSet().Return(true)
+				oldOption.EXPECT().Value().Return("old-value", nil)
+				oldOption.EXPECT().Name().Return("old-option").Times(3)
+				mergedOption.EXPECT().IsSet().Return(true).Times(2)
+				mergedOption.EXPECT().Value().Return("old-value", nil)
+				mergedOption.EXPECT().Name().Return("old-option").Times(2)
+				mergedOption.EXPECT().Default().Return("default-value")
+				mergedOption.EXPECT().Help().Return("option help")
+
+				gomock.InOrder(
+					d.EXPECT().PullUpdate(instanceId, daemon.PullTarget{}).Return(daemon.PullUpdateResult{
+						Name:          "mock-avs",
+						Tag:           "default",
+						Url:           common.MockAvsPkg.Repo(),
+						Profile:       "option-returner",
+						OldVersion:    "v5.4.0",
+						NewVersion:    common.MockAvsPkg.Version(),
+						OldCommit:     "b64c50c15e53ae7afebbdbe210b834d1ee471043",
+						NewCommit:     common.MockAvsPkg.CommitHash(),
+						HasPlugin:     true,
+						OldOptions:    []daemon.Option{oldOption},
+						NewOptions:    []daemon.Option{newOption},
+						MergedOptions: []daemon.Option{mergedOption},
+						HardwareRequirements: daemon.HardwareRequirements{
+							MinCPUCores:                 2,
+							MinRAM:                      2048,
+							MinFreeSpace:                5120,
+							StopIfRequirementsAreNotMet: true,
+						},
+					}, nil),
+					d.EXPECT().Backup(instanceId).Return(fmt.Sprintf("%s-%d", instanceId, time.Now().Unix()), nil),
+					d.EXPECT().Uninstall(instanceId).Return(nil),
+					d.EXPECT().Install(daemon.InstallOptions{
+						Name:    "mock-avs",
+						Tag:     "default",
+						URL:     common.MockAvsPkg.Repo(),
+						Profile: "option-returner",
+						Version: common.MockAvsPkg.Version(),
+						Commit:  common.MockAvsPkg.CommitHash(),
+						Options: []daemon.Option{mergedOption},
+					}).Return("", assert.AnError),
+					d.EXPECT().Restore(gomock.Any(), false).Return(assert.AnError),
+				)
+			},
+		},
+		{
 			name: "invalid arguments, instance id is required",
 			args: []string{},
 			err:  fmt.Errorf("%w: instance-id is required", ErrInvalidNumberOfArgs),

97d5c6c1b7d5781b2d9c5e6535e4088ac12c519b
AntiD2ta <mtpotro41@gmail.com>
2023-10-06 15:31:45 +0000

    ci: Update release pipeline
    Changes:
    - Remove kinetic distro and publish to lunar instead
    - Use main branch instead of default one

--- scripts/copy-ppa-packages-to-new-series.sh
+++ scripts/copy-ppa-packages-to-new-series.sh
@@ -6,7 +6,7 @@ sudo apt-get install ubuntu-dev-tools -y
 cd ubuntu-archive-tools
 echo 'Copying Packages'
 python3 copy-package -y -b -p nethermindeth --ppa-name=eigenlayer -s jammy --to-suite=focal eigenlayer
-python3 copy-package -y -b -p nethermindeth --ppa-name=eigenlayer -s jammy --to-suite=kinetic eigenlayer
+python3 copy-package -y -b -p nethermindeth --ppa-name=eigenlayer -s jammy --to-suite=lunar eigenlayer
 python3 copy-package -y -b -p nethermindeth --ppa-name=eigenlayer -s jammy --to-suite=bionic eigenlayer
 cd ..
 echo 'Cleanup'

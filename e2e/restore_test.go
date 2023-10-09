18ee2b9bc6c71afb32a04b27d9838544bc9d65e8
adriantpaez <adriantpaez@gmail.com>
2023-10-09 10:59:46 +0000

    test: improve restore e2e tests
    - check env file values
    - check state.json value
    - restore after installing same instance but different values

--- e2e/restore_test.go
+++ e2e/restore_test.go
@@ -1,19 +1,23 @@
 package e2e
 
 import (
+	"encoding/json"
 	"regexp"
 	"testing"
 
 	"github.com/NethermindEth/eigenlayer/internal/common"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/maps"
 )
 
 func TestRestore(t *testing.T) {
 	// Test context
 	var (
-		backupId   string
-		restoreErr error
+		instanceState json.RawMessage
+		instanceEnv   map[string]string
+		backupId      string
+		restoreErr    error
 	)
 	// Build test case
 	e2eTest := newE2ETestCase(
@@ -30,6 +34,10 @@ func TestRestore(t *testing.T) {
 			if err != nil {
 				return err
 			}
+			// Save instance env
+			instanceEnv = loadEnv(t, "mock-avs-default")
+			// Save instance state
+			instanceState = loadStateJSON(t, "mock-avs-default")
 			// Backup AVS
 			backupOut, err := runCommandOutput(t, eigenlayerPath, "backup", "mock-avs-default")
 			if err != nil {
@@ -51,6 +59,8 @@ func TestRestore(t *testing.T) {
 		func(t *testing.T) {
 			assert.NoError(t, restoreErr, "restore command should not fail")
 			checkInstanceExists(t, "mock-avs-default")
+			checkEnvValues(t, "mock-avs-default", instanceEnv)
+			checkStateJson(t, "mock-avs-default", instanceState)
 		},
 	)
 	// Run test case
@@ -60,8 +70,10 @@ func TestRestore(t *testing.T) {
 func TestRestore_InstanceExists(t *testing.T) {
 	// Test context
 	var (
-		backupId   string
-		restoreErr error
+		instanceState json.RawMessage
+		instanceEnv   map[string]string
+		backupId      string
+		restoreErr    error
 	)
 	// Build test case
 	e2eTest := newE2ETestCase(
@@ -78,6 +90,10 @@ func TestRestore_InstanceExists(t *testing.T) {
 			if err != nil {
 				return err
 			}
+			// Save instance env
+			instanceEnv = loadEnv(t, "mock-avs-default")
+			// Save instance state
+			instanceState = loadStateJSON(t, "mock-avs-default")
 			// Backup AVS
 			backupOut, err := runCommandOutput(t, eigenlayerPath, "backup", "mock-avs-default")
 			if err != nil {
@@ -88,6 +104,20 @@ func TestRestore_InstanceExists(t *testing.T) {
 			matches := r.FindSubmatch(backupOut)
 			require.Len(t, matches, 2)
 			backupId = string(matches[1])
+			// Uninstall AVS instance
+			err = runCommand(t, eigenlayerPath, "uninstall", "mock-avs-default")
+			if err != nil {
+				return err
+			}
+			// Install new AVS instance, with the same id, but with different
+			// options values
+			err = runCommand(t, eigenlayerPath, "install", "--profile", "option-returner", "--no-prompt", "--version", common.MockAvsPkg.Version(), "--option.test-option-hidden", "87654321", "--option.test-option-enum-hidden", "option1", common.MockAvsPkg.Repo())
+			if err != nil {
+				return err
+			}
+			// Check that the new instance env is different from the old one
+			newInstanceEnv := loadEnv(t, "mock-avs-default")
+			require.False(t, maps.Equal(instanceEnv, newInstanceEnv), "instance env should be different")
 			return nil
 		},
 		// Act
@@ -98,6 +128,8 @@ func TestRestore_InstanceExists(t *testing.T) {
 		func(t *testing.T) {
 			assert.NoError(t, restoreErr, "restore command should not fail")
 			checkInstanceExists(t, "mock-avs-default")
+			checkEnvValues(t, "mock-avs-default", instanceEnv)
+			checkStateJson(t, "mock-avs-default", instanceState)
 		},
 	)
 	// Run test case
@@ -107,8 +139,10 @@ func TestRestore_InstanceExists(t *testing.T) {
 func TestRestore_InstanceExistsRunning(t *testing.T) {
 	// Test context
 	var (
-		backupId   string
-		restoreErr error
+		instanceState json.RawMessage
+		instanceEnv   map[string]string
+		backupId      string
+		restoreErr    error
 	)
 	// Build test case
 	e2eTest := newE2ETestCase(
@@ -125,6 +159,10 @@ func TestRestore_InstanceExistsRunning(t *testing.T) {
 			if err != nil {
 				return err
 			}
+			// Save instance env
+			instanceEnv = loadEnv(t, "mock-avs-default")
+			// Save instance state
+			instanceState = loadStateJSON(t, "mock-avs-default")
 			// Backup AVS
 			backupOut, err := runCommandOutput(t, eigenlayerPath, "backup", "mock-avs-default")
 			if err != nil {
@@ -135,7 +173,21 @@ func TestRestore_InstanceExistsRunning(t *testing.T) {
 			matches := r.FindSubmatch(backupOut)
 			require.Len(t, matches, 2)
 			backupId = string(matches[1])
-			return runCommand(t, eigenlayerPath, "run", "mock-avs-default")
+			// Uninstall AVS instance
+			err = runCommand(t, eigenlayerPath, "uninstall", "mock-avs-default")
+			if err != nil {
+				return err
+			}
+			// Install new AVS instance, with the same id, but with different
+			// options values
+			err = runCommand(t, eigenlayerPath, "install", "--profile", "option-returner", "--yes", "--no-prompt", "--version", common.MockAvsPkg.Version(), "--option.test-option-hidden", "87654321", "--option.test-option-enum-hidden", "option1", common.MockAvsPkg.Repo())
+			if err != nil {
+				return err
+			}
+			// Check that the new instance env is different from the old one
+			newInstanceEnv := loadEnv(t, "mock-avs-default")
+			require.False(t, maps.Equal(instanceEnv, newInstanceEnv), "instance env should be different")
+			return nil
 		},
 		// Act
 		func(t *testing.T, egnPath string) {
@@ -145,6 +197,8 @@ func TestRestore_InstanceExistsRunning(t *testing.T) {
 		func(t *testing.T) {
 			assert.NoError(t, restoreErr, "restore command should not fail")
 			checkInstanceExists(t, "mock-avs-default")
+			checkEnvValues(t, "mock-avs-default", instanceEnv)
+			checkStateJson(t, "mock-avs-default", instanceState)
 		},
 	)
 	// Run test case
@@ -154,8 +208,10 @@ func TestRestore_InstanceExistsRunning(t *testing.T) {
 func TestRestore_Run(t *testing.T) {
 	// Test context
 	var (
-		backupId   string
-		restoreErr error
+		instanceState json.RawMessage
+		instanceEnv   map[string]string
+		backupId      string
+		restoreErr    error
 	)
 	// Build test case
 	e2eTest := newE2ETestCase(
@@ -172,6 +228,10 @@ func TestRestore_Run(t *testing.T) {
 			if err != nil {
 				return err
 			}
+			// Save instance env
+			instanceEnv = loadEnv(t, "mock-avs-default")
+			// Save instance state
+			instanceState = loadStateJSON(t, "mock-avs-default")
 			// Backup AVS
 			backupOut, err := runCommandOutput(t, eigenlayerPath, "backup", "mock-avs-default")
 			if err != nil {
@@ -193,6 +253,8 @@ func TestRestore_Run(t *testing.T) {
 		func(t *testing.T) {
 			assert.NoError(t, restoreErr, "restore command should not fail")
 			checkInstanceExists(t, "mock-avs-default")
+			checkEnvValues(t, "mock-avs-default", instanceEnv)
+			checkStateJson(t, "mock-avs-default", instanceState)
 			optionReturnerIP, err := getContainerIPByName("option-returner", "eigenlayer")
 			require.NoError(t, err, "failed to get option-returner container IP")
 			checkAVSHealth(t, optionReturnerIP, "8080", 200)
